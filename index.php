9577824c473c3a8676434a8ccdfb642fae9cdb76
Complexityy <gamingpackage@gmail.com>
2015-04-05 15:34:54 +0200

    The file that puts it all together

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-EN">
+
+	<head>
+	
+		<title>HackflagCMS</title>
+		<link href="css/legacy.css" rel="stylesheet" type="text/css"> <!-- Legacy Style, only here for now. -->
+		<link href="css/style.css" rel="stylesheet" type="text/css"> <!-- Main style file, importante! -->
+		
+	</head>
+	
+	<body>
+
+		<h1>HackflagCMS</h1>
+		<h3>https://hackflag.org/forum/showthread.php?tid=4662&pid=62147</h3>
+
+	</body>
+	
+</html>
