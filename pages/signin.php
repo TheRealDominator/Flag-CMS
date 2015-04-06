b1ef0c1e86ea277047eaa83f880dd89199618611
Complexityy <gamingpackage@gmail.com>
2015-04-06 14:07:08 +0200

    New folder
    Added /pages/ with signin.php & register.php

---
+++ pages/signin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en-EN">
+
+	<head>
+	
+		<title>HackflagCMS</title>
+		<link href="../css/style.css" rel="stylesheet" type="text/css"> <!-- Main style file, importante! -->
+		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		
+	</head>
+	
+	<body class="body">
+
+	<div class="jumbotron vertical-center">
+		<div class="container">
+
+			<div class="vertical-center-row">
+			  <form width="10%" class="form-signin" autocomplete='off' />
+				<h2 class="form-signin-heading">HackflagCMS</h2>
+				<hr><hr><hr>
+				<label for="inputEmail" class="sr-only">Email address</label>
+				<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" autocomplete='off' />
+				<label for="inputPassword" class="sr-only">Password</label>
+				<br>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" autocomplete='off' />
+				<hr>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in to your account</button>
+			  </form>
+
+			</div>
+		</div>
+	</div>
+
+		<!------= Don't touch below this line =------>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+	
+</html>
