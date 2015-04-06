41687578cc7a200c2dd359193e76d9c2754ce087
Complexityy <gamingpackage@gmail.com>
2015-04-06 16:52:46 +0200

    Bootstrap.php fix

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
-include 'config.php';
+include 'config.php'; // Include config file with variables.
 
-define('SITE_URL', $siteurl); //.. Just an example
+define('CMS_NAME', $cmsName); // Define constant preventing API's from overwritting.
+unset($cmsName);
+
+define('SITE_TITLE', $siteTitle); // Define constant ...
+unset($siteTitle);
+
+define('SITE_URL', $siteurl); // Define constant ...
 unset($siteurl);
+
+?>
